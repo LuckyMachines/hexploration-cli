7a62967b564be5e8f64c29e09ff621a499cd7ed1
Max Meier <max@studiomax.io>
2022-05-13 12:13:06 -0700

    contract addresses example

---
+++ settings/ContractAddresses-example.js
@@ -0,0 +1,48 @@
+// Change the name of this to ContractAddresses.js and set your locally deployed addresses
+const contractAddresses = {
+  // in lm-game-workshop/hexploration/resources.json
+  GANACHE_HEXPLORATION_BOARD: "",
+  GANACHE_HEXPLORATION_CONTROLLER: "",
+  GANACHE_PLAYER_REGISTRY: "",
+  // in lm-game-workshop/settings/addresses.json
+  GANACHE_GAME_REGISTRY: "",
+  // copy from command line during deployments
+  GANACHE_HEXPLORATION_RULES: "",
+  GANACHE_HEXPLORATION_ZONE: "",
+  GANACHE_AMBUSH_DECK: "",
+  GANACHE_EVENT_DECK: "",
+  GANACHE_LAND_DECK: "",
+  GANACHE_TREASURE_DECK: "",
+  GANACHE_DAY_NIGHT_TOKEN: "",
+  GANACHE_DISASTER_TOKEN: "",
+  GANACHE_ENEMY_TOKEN: "",
+  GANACHE_ITEM_TOKEN: "",
+  GANACHE_PLAYER_STATUS_TOKEN: "",
+  GANACHE_BOARD_WALLET: "",
+  GANACHE_PLAYER_WALLET: "",
+  GANACHE_ZONE_WALLET: "",
+  GANACHE_CHARACTER_CARD: ""
+
+  //ADD DEPLOYED CONTRACTS TO PLAY ON MORE CHAINS
+  // _HEXPLORATION_BOARD: "",
+  // _HEXPLORATION_CONTROLLER: "",
+  // _HEXPLORATION_RULES: "",
+  // _HEXPLORATION_ZONE: "",
+  // _AMBUSH_DECK: "",
+  // _EVENT_DECK: "",
+  // _LAND_DECK: "",
+  // _TREASURE_DECK: "",
+  // _BOARD_WALLET: "",
+  // _PLAYER_WALLET: "",
+  // _ZONE_WALLET: "",
+  // _CHARACTER_CARD: "",
+  // _DAY_NIGHT_TOKEN: "",
+  // _DISASTER_TOKEN: "",
+  // _ENEMY_TOKEN: "",
+  // _ITEM_TOKEN: "",
+  // _PLAYER_STATUS_TOKEN: "",
+  // _GAME_REGISTRY: "",
+  // _PLAYER_REGISTRY: ""
+};
+
+module.exports = contractAddresses;
