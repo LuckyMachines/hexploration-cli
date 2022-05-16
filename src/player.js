c818df6d671a90c63f56896de3da887a4456cf8d
Max Meier <max@studiomax.io>
2022-05-15 20:47:46 -0700

    display player stats

--- src/player.js
+++ src/player.js
@@ -9,11 +9,38 @@ let board;
 export async function playerInfo(gameID, provider, account) {
   const accounts = await provider.eth.getAccounts();
   currentAccount = account ? account : accounts[0];
-  console.log("Getting player info for game:", gameID);
+  // console.log("Getting player info for game:", gameID);
   summary = await Contract("summary", provider);
   board = await Contract("board", provider);
   let playerStats = await summary.methods
     .currentPlayerStats(board._address, gameID)
     .call();
-  console.log(playerStats);
+  let activeInventory = await summary.methods
+    .activeInventory(board._address, gameID)
+    .call();
+  let handInventory = await summary.methods
+    .currentHandInventory(board._address, gameID)
+    .call();
+
+  console.log(
+    `Movement: ${playerStats.movement}, Agility: ${playerStats.agility}, Dexterity: ${playerStats.dexterity}`
+  );
+  console.log("Campsite:", activeInventory.campsite ? "Packed Up" : "On Board");
+  console.log(
+    `Left hand item: ${
+      handInventory.leftHandItem ? handInventory.leftHandItem : "None"
+    }, Right hand item:${
+      handInventory.rightHandItem ? handInventory.rightHandItem : "None"
+    }`
+  );
+  console.log(
+    "Player Status:",
+    activeInventory.status ? activeInventory.status : "Healthy"
+  );
+  console.log(
+    "Artifact:",
+    activeInventory.artifact ? activeInventory.artifact : "None"
+  );
+  console.log("Relic:", activeInventory.relic ? activeInventory.relic : "None");
+  console.log("Shield:", activeInventory.shield ? "Enabled" : "None");
 }
