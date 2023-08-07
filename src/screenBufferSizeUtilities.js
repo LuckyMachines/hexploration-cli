c399ac1a79c77648b368aa49d764022ce6267829
James Pollack <james.b.pollack@gmail.com>
2023-08-06 22:03:42 -0700

    make ref-napi external and include it in assets and it will build esbuild AND pkg

---
+++ src/screenBufferSizeUtilities.js
@@ -0,0 +1,93 @@
+var ref = require("ref-napi");
+// var bigint = require("bigint");
+
+// define the "type" instance according to the spec
+var BigintType = {
+  size: ref.sizeof.int64,
+  //   indirection: 1,
+  //   get: function (buffer, offset) {
+  //     // return a bigint instance from the buffer
+  //     return bigint.fromBuffer(buffer);
+  //   },
+  //   set: function (buffer, offset, value) {
+  //     // 'value' would be a bigint instance
+  //     var val = value.toString();
+  //     return ref.writeInt64(buffer, offset || 0, val);
+  //   },
+};
+// /// set console size on windows
+// const ffi = require("ffi-napi");
+// const ref = require("ref-napi");
+// const StructType = require("ref-struct-di")(ref);
+// const os = require("os");
+
+// const BOOL = "int32";
+
+// const COORD = StructType({
+//   X: "short",
+//   Y: "short",
+// });
+
+// const SMALL_RECT = StructType({
+//   Left: "short",
+//   Top: "short",
+//   Right: "short",
+//   Bottom: "short",
+// });
+
+// const CONSOLE_SCREEN_BUFFER_INFO = StructType({
+//   dwSize: COORD,
+//   dwCursorPosition: COORD,
+//   wAttributes: "ushort",
+//   srWindow: SMALL_RECT,
+//   dwMaximumWindowSize: COORD,
+// });
+
+// const kernel32 = ffi.Library("kernel32.dll", {
+//   GetStdHandle: ["pointer", ["int32"]],
+//   GetConsoleScreenBufferInfo: [BOOL, ["pointer", "pointer"]],
+//   SetConsoleScreenBufferSize: ["int32", ["pointer", COORD]],
+// });
+
+// const STD_OUTPUT_HANDLE = -11;
+
+// function setConsoleBufferHeightWindows(height) {
+//   const handle = kernel32.GetStdHandle(STD_OUTPUT_HANDLE);
+//   if (ref.isNull(handle)) {
+//     console.error("Failed to get standard output handle.");
+//     return;
+//   }
+
+//   const info = new CONSOLE_SCREEN_BUFFER_INFO();
+//   if (!kernel32.GetConsoleScreenBufferInfo(handle, info.ref())) {
+//     console.error("Failed to get console screen buffer info.");
+//     return;
+//   }
+
+//   info.dwSize.Y = height;
+
+//   if (!kernel32.SetConsoleScreenBufferSize(handle, info.dwSize)) {
+//     console.error("Failed to set console screen buffer size.");
+//     return;
+//   }
+
+//   console.log(`Console buffer height set to ${height}`);
+// }
+
+// const detectOS = () => {
+//   switch (os.platform()) {
+//     case "win32":
+//       return "windows";
+//     case "darwin":
+//       return "mac";
+//     case "linux":
+//       return "linux";
+
+//     default:
+//       return "something else";
+//   }
+// };
+
+// // // Example usage:
+// // setConsoleBufferHeight(200);
+// export { detectOS, setConsoleBufferHeightWindows };
